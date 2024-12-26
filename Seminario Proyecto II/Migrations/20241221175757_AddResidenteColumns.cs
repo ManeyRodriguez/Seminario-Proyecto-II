a2a55e1d4c110d78b58d078b70d44ce4aae3197a
ethicsDev <developers@fragmento.do>
2024-12-26 10:17:04 -0400

    actualizciones migraciones del proyecto

---
+++ Seminario Proyecto II/Migrations/20241221175757_AddResidenteColumns.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Seminario_Proyecto_II.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResidenteColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nombre",
+                table: "Residentes",
+                newName: "Nombres");
+
+            migrationBuilder.RenameColumn(
+                name: "Nombre",
+                table: "PersonasRelacionadas",
+                newName: "Nombres");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Apellidos",
+                table: "Residentes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DocID",
+                table: "Residentes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Tipo",
+                table: "PersonasRelacionadas",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Apellidos",
+                table: "PersonasRelacionadas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TipoAcceso",
+                table: "HistorialDeAccesos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Resultado",
+                table: "HistorialDeAccesos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Tipo",
+                table: "CodigosDeAcceso",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Apellidos",
+                table: "Residentes");
+
+            migrationBuilder.DropColumn(
+                name: "DocID",
+                table: "Residentes");
+
+            migrationBuilder.DropColumn(
+                name: "Apellidos",
+                table: "PersonasRelacionadas");
+
+            migrationBuilder.RenameColumn(
+                name: "Nombres",
+                table: "Residentes",
+                newName: "Nombre");
+
+            migrationBuilder.RenameColumn(
+                name: "Nombres",
+                table: "PersonasRelacionadas",
+                newName: "Nombre");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tipo",
+                table: "PersonasRelacionadas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TipoAcceso",
+                table: "HistorialDeAccesos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Resultado",
+                table: "HistorialDeAccesos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tipo",
+                table: "CodigosDeAcceso",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
