5a0d0acc2b04947be2369a5c04dcd110fa6a3789
ethicsDev <developers@fragmento.do>
2024-12-26 10:15:14 -0400

    modificaciones del modelo residente

--- Seminario Proyecto II/Data/Models/Residente.cs
+++ Seminario Proyecto II/Data/Models/Residente.cs
@@ -1,26 +1,41 @@
-﻿using System;
+﻿using Seminario_Proyecto_II.Data.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace Seminario_Proyecto_II.Data.Models
-{
-    public class Residente
-    {            
-        public int Id { get; set; }
-        public string Nombres { get; set; }
-        public string Apellidos { get; set; }
-        public string DocID { get; set; }
-        public string Correo { get; set; }
-        public string Tel { get; set; }
-        public string PassHash { get; set; }
-        public DateTime Fecha { get; set; } = DateTime.Now;
-        public string Estado { get; set; } // Activo o Inactivo
+public class Residente
+{    
+    public int Id { get; set; }
 
-        public ICollection<Casa> Casas { get; set; }
-        public ICollection<PersonaRelacionada> PersonasRelacionadas { get; set; }
-        public ICollection<Notificacion> Notificaciones { get; set; }
-    }
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(50, ErrorMessage = "El nombre no puede exceder los 100 caracteres.")]
+    public string Nombres { get; set; }
 
+    [Required(ErrorMessage = "El apellido es obligatorio.")]
+    [StringLength(50, ErrorMessage = "El apellido no puede exceder los 100 caracteres.")]
+    public string Apellidos { get; set; }
+
+    [Required(ErrorMessage = "El documento de identificación es obligatorio.")]
+    [StringLength(20, ErrorMessage = "El documento de identificación no puede exceder los 20 caracteres.")]
+    public string DocID { get; set; }
+
+    [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
+    [StringLength(100, ErrorMessage = "El correo no puede exceder los 100 caracteres.")]
+    public string Correo { get; set; }
+
+    [StringLength(11, ErrorMessage = "El teléfono no puede exceder los 11 caracteres.")]    
+    public string Tel { get; set; }
+
+    [Required(ErrorMessage = "La contraseña es obligatoria.")]
+    [StringLength(24, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 24 caracteres.")]
+    public string PassHash { get; set; }
+        
+    public string Estado { get; set; } = "Activo";
+
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    public ICollection<Casa> Casas { get; set; } = new List<Casa>();
+    public ICollection<PersonaRelacionada> PersonasRelacionadas { get; set; } = new List<PersonaRelacionada>();
+    public ICollection<Notificacion> Notificaciones { get; set; } = new List<Notificacion>();
 }
