3d6f3948e75b47f6bee4f2f09fe2688e02aae080
ManeyRodriguez <maneyrodriguez@gmail.com>
2025-01-14 18:31:44 -0400

    form de casa actualizado

--- Seminario Proyecto II/Forms/Casas/AgregarCasa.Designer.cs
+++ Seminario Proyecto II/Forms/Casas/AgregarCasa.Designer.cs
@@ -34,27 +34,30 @@ namespace Seminario_Proyecto_II.Forms.Casas
             // 
             // txtCalle
             // 
-            txtCalle.Location = new Point(59, 64);
+            txtCalle.Location = new Point(84, 107);
+            txtCalle.Margin = new Padding(4, 5, 4, 5);
             txtCalle.MaxLength = 100;
             txtCalle.Name = "txtCalle";
-            txtCalle.Size = new Size(300, 23);
+            txtCalle.Size = new Size(427, 31);
             txtCalle.TabIndex = 0;
             // 
             // txtNumCasa
             // 
-            txtNumCasa.Location = new Point(59, 139);
+            txtNumCasa.Location = new Point(84, 232);
+            txtNumCasa.Margin = new Padding(4, 5, 4, 5);
             txtNumCasa.MaxLength = 10;
             txtNumCasa.Name = "txtNumCasa";
-            txtNumCasa.Size = new Size(112, 23);
+            txtNumCasa.Size = new Size(158, 31);
             txtNumCasa.TabIndex = 1;
             // 
             // cmbTipo
             // 
             cmbTipo.Items.AddRange(new object[] { "Casa", "Apartamento" });
-            cmbTipo.Location = new Point(228, 139);
+            cmbTipo.Location = new Point(326, 232);
+            cmbTipo.Margin = new Padding(4, 5, 4, 5);
             cmbTipo.MaxLength = 20;
             cmbTipo.Name = "cmbTipo";
-            cmbTipo.Size = new Size(131, 23);
+            cmbTipo.Size = new Size(185, 33);
             cmbTipo.TabIndex = 2;
             // 
             // btnGuardar
@@ -62,9 +65,10 @@ namespace Seminario_Proyecto_II.Forms.Casas
             btnGuardar.BackColor = SystemColors.Highlight;
             btnGuardar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
             btnGuardar.ForeColor = SystemColors.ControlLightLight;
-            btnGuardar.Location = new Point(259, 237);
+            btnGuardar.Location = new Point(370, 395);
+            btnGuardar.Margin = new Padding(4, 5, 4, 5);
             btnGuardar.Name = "btnGuardar";
-            btnGuardar.Size = new Size(100, 28);
+            btnGuardar.Size = new Size(143, 47);
             btnGuardar.TabIndex = 4;
             btnGuardar.Text = "Guardar";
             btnGuardar.UseVisualStyleBackColor = false;
@@ -75,9 +79,10 @@ namespace Seminario_Proyecto_II.Forms.Casas
             btnLimpiar.BackColor = Color.Red;
             btnLimpiar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
             btnLimpiar.ForeColor = SystemColors.ControlLightLight;
-            btnLimpiar.Location = new Point(59, 237);
+            btnLimpiar.Location = new Point(84, 395);
+            btnLimpiar.Margin = new Padding(4, 5, 4, 5);
             btnLimpiar.Name = "btnLimpiar";
-            btnLimpiar.Size = new Size(100, 28);
+            btnLimpiar.Size = new Size(143, 47);
             btnLimpiar.TabIndex = 5;
             btnLimpiar.Text = "Limpiar";
             btnLimpiar.UseVisualStyleBackColor = false;
@@ -87,9 +92,10 @@ namespace Seminario_Proyecto_II.Forms.Casas
             // 
             lblCalle.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblCalle.ForeColor = SystemColors.ControlLightLight;
-            lblCalle.Location = new Point(59, 38);
+            lblCalle.Location = new Point(84, 63);
+            lblCalle.Margin = new Padding(4, 0, 4, 0);
             lblCalle.Name = "lblCalle";
-            lblCalle.Size = new Size(100, 23);
+            lblCalle.Size = new Size(143, 38);
             lblCalle.TabIndex = 0;
             lblCalle.Text = "Calle:";
             // 
@@ -97,9 +103,10 @@ namespace Seminario_Proyecto_II.Forms.Casas
             // 
             lblNumCasa.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblNumCasa.ForeColor = SystemColors.ControlLightLight;
-            lblNumCasa.Location = new Point(59, 114);
+            lblNumCasa.Location = new Point(84, 190);
+            lblNumCasa.Margin = new Padding(4, 0, 4, 0);
             lblNumCasa.Name = "lblNumCasa";
-            lblNumCasa.Size = new Size(100, 23);
+            lblNumCasa.Size = new Size(197, 38);
             lblNumCasa.TabIndex = 1;
             lblNumCasa.Text = "Número de Casa:";
             // 
@@ -107,18 +114,19 @@ namespace Seminario_Proyecto_II.Forms.Casas
             // 
             lblTipo.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblTipo.ForeColor = SystemColors.ControlLightLight;
-            lblTipo.Location = new Point(228, 113);
+            lblTipo.Location = new Point(326, 188);
+            lblTipo.Margin = new Padding(4, 0, 4, 0);
             lblTipo.Name = "lblTipo";
-            lblTipo.Size = new Size(100, 23);
+            lblTipo.Size = new Size(143, 38);
             lblTipo.TabIndex = 2;
             lblTipo.Text = "Tipo:";
             // 
             // AgregarCasa
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(34, 34, 34);
-            ClientSize = new Size(420, 300);
+            ClientSize = new Size(600, 500);
             Controls.Add(lblCalle);
             Controls.Add(lblNumCasa);
             Controls.Add(lblTipo);
@@ -128,6 +136,7 @@ namespace Seminario_Proyecto_II.Forms.Casas
             Controls.Add(btnGuardar);
             Controls.Add(btnLimpiar);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Margin = new Padding(4, 5, 4, 5);
             Name = "AgregarCasa";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Agregar Casa";
